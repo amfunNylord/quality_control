3f4fc27050cad5f08196877d3dd990ca9ef83c78
amfunNyLORD <funnybulletgames@yandex.ru>
2024-05-08 20:16:46 +0300

    added fixes for lab 8

--- lab8/tests/api-tests.spec.js
+++ lab8/tests/api-tests.spec.js
@@ -1,16 +1,11 @@
-const axios = require('axios');
 const fs = require('fs');
 const Ajv = require('ajv');
+const ProductApi = require('./product_api');
 
-const BASE_URL = "http://shop.qatl.ru/";
-const LIST_PRODUCTS_URI = 'api/products';
-const ADD_PRODUCT_URI = 'api/addproduct';
-const EDIT_PRODUCT_URI = 'api/editproduct';
-const DELETE_PRODUCT_URI = 'api/deleteproduct';
 const testData = JSON.parse(fs.readFileSync('config/test-values.json', 'utf-8'));
 const schema = JSON.parse(fs.readFileSync('config/schema.json', 'utf8'));
 
-let addedProductId;
+const productApi = new ProductApi();
 const ajv = new Ajv();
 const validate = ajv.compile(schema);
 
@@ -20,320 +15,180 @@ function CheckProduct(obj1, obj2) {
     return jsonString1 == jsonString2;
 }
 
-async function addProductRequest(product, status) {
-    const addProductResponse = await axios.post(`${BASE_URL + ADD_PRODUCT_URI}`, product);
-    expect(addProductResponse.data.status).toBe(status);
-    if (status != undefined) {
-        addedProductId = addProductResponse.data.id;
-    }
+function addingProductTestFailure(testName, productData, status) {
+    test(testName, async () => {
+        const allProductsCount = await productApi.getProductsCount();
+
+        await productApi.addProductRequest(productData, status);
+
+        const allProductsCountNew =  await productApi.getProductsCount();
+
+        expect(allProductsCountNew).toBe(allProductsCount);
+    }, 10000);
 }
 
-async function deleteProductRequest(productId, status) {
-    const deleteProductResponse = await axios.get(`${BASE_URL + DELETE_PRODUCT_URI}?id=${productId}`);
-    expect(deleteProductResponse.data.status).toBe(status);
+function addingProductTestSuccess(testName, productData) {
+    test(testName, async () => {
+        const allProductsCount = await productApi.getProductsCount();
+
+        await productApi.addProductRequest(productData, 1);
+
+        await productApi.deleteProductRequest(productApi.addedProductId, 1);
+
+        const allProductsCountNew = await productApi.getProductsCount();
+        expect(allProductsCountNew).toBe(allProductsCount);
+    }, 10000);
 }
 
-async function editProductRequest(product, status) {
-    const editProductResponse = await axios.post(`${BASE_URL + EDIT_PRODUCT_URI}`, product);
-    expect(editProductResponse.data.status).toBe(status);
+function deleteProductTestFailure(testName, id) {
+    test(testName, async () => {
+        await productApi.deleteProductRequest(id, 0);
+    });
 }
 
-async function getAllProductRequest() {
-    const getAllProductsResponse = await axios.get(`${BASE_URL + LIST_PRODUCTS_URI}`);
-    expect(getAllProductsResponse.status).toBe(200);
-    return getAllProductsResponse.data;
-}
-
-// вынести в отдельные методы, использовать global айдишник, добавить схему
 describe("Testing shop api", () => {
     // Test cases go here
     test('Adding correct product and deleting it', async () => {
         const productData = testData.correctProduct;
-        await addProductRequest(productData, 1);
+        await productApi.addProductRequest(productData, 1);
 
-        const allProductsResponse = await getAllProductRequest();
-        const allProductsCount = allProductsResponse.length;
-        const addedProduct = allProductsResponse[allProductsCount - 1];
-
+        const addedProduct = await productApi.getLastAddedProduct();
         expect(validate(addedProduct)).toBe(true);
         expect(CheckProduct(addedProduct, productData)).toBe(true);
 
-        await deleteProductRequest(addedProductId, 1);
+        await productApi.deleteProductRequest(productApi.addedProductId, 1);
 
-        const allProductsResponseNew = await getAllProductRequest();
-        const allProductsCountNew = allProductsResponseNew.length;
-        expect(allProductsCountNew + 1).toBe(allProductsCount);
-    });
+        const allProductsCountNew =  await productApi.getProductsCount();
+        expect(allProductsCountNew + 1).toBe(productApi.allProductsCount);
+    }, 10000);
     test('Adding correct product with 15 category', async () => {
         // берем тестовые данные
         const productData = testData.productWithValid15Category;
 
-        await addProductRequest(productData, 1);
+        await productApi.addProductRequest(productData, 1);
 
-        const allProductsResponse = await getAllProductRequest();
-        const allProductsCount = allProductsResponse.length;
-        const addedProduct = allProductsResponse[allProductsCount - 1];
+        const addedProduct = await productApi.getLastAddedProduct();
         expect(validate(addedProduct)).toBe(true);
 
-        await deleteProductRequest(addedProductId, 1);
+        await productApi.deleteProductRequest(productApi.addedProductId, 1);
 
-        const allProductsResponseNew = await getAllProductRequest();
-        const allProductsCountNew = allProductsResponseNew.length;
-        expect(allProductsCountNew).toBe(allProductsCount);
+        const allProductsCountNew =  await productApi.getProductsCount();
+        expect(allProductsCountNew).toBe(productApi.allProductsCount);
     }, 10000);
 
     test('Adding correct product with same alias', async () => {
         // берем тестовые данные
         const productData = testData.correctProduct;
 
-        await addProductRequest(productData, 1);
-        const addedProductId1 = addedProductId;
-
-        await addProductRequest(productData, 1);
-        const addedProductId2 = addedProductId;
-
-        const allProductsResponse = await getAllProductRequest();
-        const allProductsCount = allProductsResponse.length;
-        const addedProduct1 = allProductsResponse[allProductsCount - 2];
-        const addedProduct2 = allProductsResponse[allProductsCount - 1];
+        await productApi.addProductRequest(productData, 1);
+        const addedProductId1 = productApi.addedProductId;
+        const addedProduct1 = await productApi.getLastAddedProduct();
         expect(validate(addedProduct1)).toBe(true);
+
+        await productApi.addProductRequest(productData, 1);
+        const addedProductId2 = productApi.addedProductId;
+        const addedProduct2 = await productApi.getLastAddedProduct();
         expect(validate(addedProduct2)).toBe(true);
 
         expect(addedProduct1.alias).toBe(productData.alias);
         expect(addedProduct2.alias).toBe(productData.alias + '-0');
 
-        await deleteProductRequest(addedProductId1, 1);
-        await deleteProductRequest(addedProductId2, 1);
+        await productApi.deleteProductRequest(addedProductId1, 1);
+        await productApi.deleteProductRequest(addedProductId2, 1);
     }, 10000);
 
-    test('Adding incorrect product with invalid price', async () => {
-        const productData = testData.productWithStringPrice;
+    addingProductTestFailure('Adding incorrect product with invalid price', testData.productWithStringPrice, undefined);
+    addingProductTestFailure('Adding incorrect product with negative price', testData.productWithNegativePrice, undefined);
+    addingProductTestFailure('Adding incorrect product with null category', testData.productWithNullCategory, undefined);
+    addingProductTestFailure('Adding incorrect product with missing props', testData.productWithMissingProps, undefined);
+    addingProductTestFailure('Adding empty product', testData.emptyProduct, undefined);
+    addingProductTestFailure('Adding null product', testData.nullProduct, 0);
 
-        // добавляем товар
-        await addProductRequest(productData, undefined);
-    });
+    addingProductTestSuccess('Adding incorrect product with invalid 0 category', testData.productWithInvalid0Category);
+    addingProductTestSuccess('Adding incorrect product with invalid 16 category', testData.productWithInvalid16Category);
+    addingProductTestSuccess('Adding incorrect product with null status', testData.productWithNullStatus);
+    addingProductTestSuccess('Adding incorrect product with invalid status', testData.productWithInvalidStatus);
+    addingProductTestSuccess('Adding incorrect product with null hit', testData.productWithNullHit);
+    addingProductTestSuccess('Adding incorrect product with invalid hit', testData.productWithInvalidHit);
+    addingProductTestSuccess('Adding incorrect product with invalid id', testData.productWithInvalidId);
 
-    test('Adding incorrect product with negative price', async () => {
-        const productData = testData.productWithNegativePrice;
-
-        await addProductRequest(productData, undefined);
-    });
-
-    test('Adding incorrect product with null category', async () => {
-        const productData = testData.productWithNullCategory;
-
-        await  addProductRequest(productData, undefined);
-    });
-
-    test('Adding incorrect product with invalid category', async () => {
-        const productData = testData.productWithInvalid0Category;
-
-        await addProductRequest(productData, 1);
-
-        await deleteProductRequest(addedProductId, 1);
-    });
-
-    test('Adding incorrect product with invalid 16 category', async () => {
-        const productData = testData.productWithInvalid16Category;
-
-        await addProductRequest(productData, 1);
-
-        await deleteProductRequest(addedProductId, 1);
-    });
-
-    test('Adding incorrect product with null status', async () => {
-        const productData = testData.productWithNullStatus;
-
-        await addProductRequest(productData, 1);
-
-        await deleteProductRequest(addedProductId, 1);
-    });
-
-    test('Adding incorrect product with invalid status', async () => {
-        const productData = testData.productWithInvalidStatus;
-
-        await addProductRequest(productData, 1);
-
-        await deleteProductRequest(addedProductId, 1);
-    });
-
-    test('Adding incorrect product with null hit', async () => {
-        const productData = testData.productWithNullHit;
-
-        await addProductRequest(productData, 1);
-
-        await deleteProductRequest(addedProductId, 1);
-    });
-
-    test('Adding incorrect product with invalid hit', async () => {
-        const productData = testData.productWithInvalidHit;
-
-        await addProductRequest(productData, 1);
-
-        await deleteProductRequest(addedProductId, 1);
-    });
-
-    test('Adding incorrect product with missing props', async () => {
-        const productData = testData.productWithMissingProps;
-
-        await addProductRequest(productData, undefined);
-    });
-
-    test('Adding empty product', async () => {
-        const productData = testData.emptyProduct;
-
-        await addProductRequest(productData, undefined);
-    });
-
-    test('Adding null product', async () => {
-        const productData = testData.nullProduct;
-
-        await addProductRequest(productData, 0);
-    });
-
-    test('Adding incorrect product with invalid id', async () => {
-        const productData = testData.productWithInvalidId;
-
-        await addProductRequest(productData, 1);
-
-        await deleteProductRequest(addedProductId, 1);
-    });
-
-    test('Deleting non-existing product', async () => {
-        const nonExistingProductId = 1000000;
-
-        await deleteProductRequest(nonExistingProductId, 0);
-    });
-
-    test('Deleting product with invalid id', async () => {
-        const invalidId = "id";
-
-        await deleteProductRequest(invalidId, 0);
-    });
+    deleteProductTestFailure('Deleting non-existing product', 1000000);
+    deleteProductTestFailure('Deleting product with invalid id', "id");
 
     test('Edit existing product', async () => {
-        // берем тестовые данные
         const productData = testData.correctProduct;
+        await productApi.addProductRequest(productData, 1);
 
-        await addProductRequest(productData, 1);
-
-        // получаем все товары
-        const allProductsResponse = await getAllProductRequest();
-        const allProductsCount = allProductsResponse.length;
-        const addedProduct = allProductsResponse[allProductsCount - 1];
+        const addedProduct = await productApi.getLastAddedProduct();
         expect(validate(addedProduct)).toBe(true);
-
-        // проверяем что это именно тот товар, который мы добавили
         expect(CheckProduct(addedProduct, productData)).toBe(true);
 
         let editedProductData = testData.correctProductEdited;
-        editedProductData.id = addedProductId;
-        editProductRequest(editedProductData, 1);
-
+        editedProductData.id = productApi.addedProductId;
+        await productApi.editProductRequest(editedProductData, 1);
         delete editedProductData.id;
 
-        // получаем все товары
-
-        const allProductsResponse2 = await getAllProductRequest();
-        const allProductsCount2 = allProductsResponse2.length;
-        const editedProduct = allProductsResponse2[allProductsCount2 - 1];
+        const editedProduct = await productApi.getLastAddedProduct();
         expect(validate(editedProduct)).toBe(true);
-
-        // проверяем что товар изменился
         expect(CheckProduct(editedProduct, editedProductData)).toBe(true);
 
-        await deleteProductRequest(addedProductId, 1);
-
-        const allProductsResponseNew = await getAllProductRequest();
-        const allProductsCountNew = allProductsResponseNew.length;
-        expect(allProductsCountNew + 1).toBe(allProductsCount);
+        await productApi.deleteProductRequest(productApi.addedProductId, 1);
+        const allProductsCountNew =  await productApi.getProductsCount();
+        expect(allProductsCountNew + 1).toBe(productApi.allProductsCount);
     }, 10000);
 
     test('Edit existing product with alias already is used', async () => {
-        // берем тестовые данные
         const productDataEdited = testData.correctProductEdited;
+        await productApi.addProductRequest(productDataEdited, 1);
+        const addedEditedProductId = productApi.addedProductId;
 
-        await addProductRequest(productDataEdited, 1);
-        const addedEditedProductId = addedProductId;
-
-        // получаем все товары
-        const allProductsResponse1 = await getAllProductRequest();
-        const allProductsCount1 = allProductsResponse1.length;
-        const addedEditedProduct1 = allProductsResponse1[allProductsCount1 - 1];
+        const addedEditedProduct1 = await productApi.getLastAddedProduct();
         expect(validate(addedEditedProduct1)).toBe(true);
-
-        // проверяем что это именно тот товар, который мы добавили
         expect(CheckProduct(addedEditedProduct1, productDataEdited)).toBe(true);
-
-        // берем тестовые данные
+    
         const productData = testData.correctProduct;
+        await productApi.addProductRequest(productData, 1);
 
-        await addProductRequest(productData, 1);
-
-        // получаем все товары
-        const allProductsResponse = await getAllProductRequest();
-        const allProductsCount = allProductsResponse.length;
-        const addedProduct = allProductsResponse[allProductsCount - 1];
+        const addedProduct = await productApi.getLastAddedProduct();
         expect(validate(addedProduct)).toBe(true);
-
-        // проверяем что это именно тот товар, который мы добавили
         expect(CheckProduct(addedProduct, productData)).toBe(true);
 
         let editedProductData = testData.correctProductEdited;
-        editedProductData.id = addedProductId;
-        editProductRequest(editedProductData, 1);
+        editedProductData.id = productApi.addedProductId;
+        await productApi.editProductRequest(editedProductData, 1);
 
         editedProductData.alias += `-${editedProductData.id}`;
         delete editedProductData.id;
 
-        // получаем все товары
-        const allProductsResponse2 = await getAllProductRequest();
-        const allProductsCount2 = allProductsResponse2.length;
-        const editedProduct = allProductsResponse2[allProductsCount2 - 1];
+        const editedProduct = await productApi.getLastAddedProduct();
         expect(validate(editedProduct)).toBe(true);
-
-        // проверяем что товар изменился
         expect(CheckProduct(editedProduct, editedProductData)).toBe(true);
 
-        deleteProductRequest(addedEditedProductId, 1);
-        deleteProductRequest(addedProductId, 1);
+        await productApi.deleteProductRequest(addedEditedProductId, 1);
+        await productApi.deleteProductRequest(productApi.addedProductId, 1);
 
-        const allProductsResponseNew = await getAllProductRequest();
-        const allProductsCountNew = allProductsResponseNew.length;
-        expect(allProductsCountNew + 2).toBe(allProductsCount2);
+        const allProductsCountNew = await productApi.getProductsCount();
+        expect(allProductsCountNew + 2).toBe(productApi.allProductsCount);
     }, 15000);
 
     test('Edit only price of existing product', async () => {
-        // берем тестовые данные
         const productData = testData.correctProduct;
+        await productApi.addProductRequest(productData, 1);
 
-        await addProductRequest(productData, 1);
-
-        // получаем все товары
-        const allProductsResponse = await getAllProductRequest();
-        const allProductsCount = allProductsResponse.length;
-        const addedProduct = allProductsResponse[allProductsCount - 1];
+        const addedProduct = await productApi.getLastAddedProduct();
         expect(validate(addedProduct)).toBe(true);
-
-        // проверяем что это именно тот товар, который мы добавили
         expect(CheckProduct(addedProduct, productData)).toBe(true);
 
         let editedProductData = testData.editPriceProduct;
-        editedProductData.id = addedProductId;
-        editProductRequest(editedProductData, undefined);
+        editedProductData.id = productApi.addedProductId;
+        await productApi.editProductRequest(editedProductData, undefined);
 
-        // получаем все товары
-        const allProductsResponse2 = await getAllProductRequest();
-        const allProductsCount2 = allProductsResponse2.length;
-        const editedProduct = allProductsResponse2[allProductsCount2 - 1];
-
-        // проверяем что товар не изменился
+        const editedProduct = await productApi.getLastAddedProduct();
         expect(CheckProduct(editedProduct, productData)).toBe(true);
 
-        await deleteProductRequest(addedProductId, 1);
+        await productApi.deleteProductRequest(productApi.addedProductId, 1);
 
-        const allProductsResponseNew = await getAllProductRequest();
-        const allProductsCountNew = allProductsResponseNew.length;
-        expect(allProductsCountNew + 1).toBe(allProductsCount2);
+        const allProductsCountNew = await productApi.getProductsCount();
+        expect(allProductsCountNew + 1).toBe(productApi.allProductsCount);
     }, 15000);
 });
